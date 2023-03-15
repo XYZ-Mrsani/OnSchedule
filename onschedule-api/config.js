eed252de8edad0618d4c27a22fab68c86e3185fb
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-15 22:39:04 +0530

    adding bookings collection

--- onschedule-api/config.js
+++ onschedule-api/config.js
@@ -10,8 +10,49 @@ const firebaseConfig = {
     measurementId: "G-7CD774VPS5",
 };
 
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const User = db.collection("Users");
 
-module.exports = User;
+class Bookings {
+    constructor(id, date, nicnum, fname, lname, phone, from, to, seatnum, busnum, time) {
+        this.id = id;
+        this.date = date;
+        this.nicnum = nicnum;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.from = from;
+        this.to = to;
+        this.seatnum = seatnum;
+        this.busnum = busnum;
+        this.time = time;
+
+    }
+
+    static fromFirestoreData(doc) {
+        const data = doc.data();
+        const id = doc.id;
+        const { date, nicnum, fname, lname, phone, from, to, seatnum, busnum, time } = data;
+        return new User(id, date, nicnum, fname, lname, phone, from, to, seatnum, busnum, time);
+    }
+
+    toFirebaseData() {
+        return {
+            date: this.date,
+            nicnum: this.nicnum,
+            fname: this.fname,
+            lname: this.lname,
+            phone: this.phone,
+            from: this.from,
+            to: this.to,
+            seatnum: this.seatnum,
+            busnum: this.busnum,
+            time: this.time
+        };
+    }
+}
+
+const bookingsModel = db.collection("Bookings");
+
+module.exports = { Bookings, bookingsModel };
