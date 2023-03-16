d51c5dc062708f53f7e1bc6bc6185798f2124bf9
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-16 13:36:02 +0530

    get system date

--- onschedule-api/routes/bookings.js
+++ onschedule-api/routes/bookings.js
@@ -1,24 +1,25 @@
 const express = require('express');
 var router = express.Router();
-const { Bookings, bookingsModel } = require('../config');
+const { Bookings, bookingsModel } = require('../models/bookings.model');
 
-const date = new Date();
-let cdate = date.getFullYear();
+var date = new Date();
+let cdate = date.toISOString().slice(0, 10);
 
 router.post("/create", async (req, res) => {
-  
-    const { nicnum, fname, lname, phone, from, to, seatnum, busnum, time } = req.body;
-  
-    try {
-      const newBookings = new Bookings(null, cdate, nicnum, fname, lname, phone, from, to, seatnum, busnum, time);
-      const newBookingsDocRef = await bookingsModel.add(newBookings.toFirebaseData());
-      const newBookingsDoc = await newBookingsDocRef.get();
-  
-      res.send({ status: 200, message: 'Bookings added successfully', userDetails: newBookingsDoc.data() });
-    } catch (error) {
-      res.status(400).send({ status: 400, message: 'Unable to Add Bookings' });
-    }
-  });
+
+  const { nicnum, fname, lname, phone, from, to, seatnum, busnum, time } = req.body;
+
+  try {
+    const newBookings = new Bookings(null, cdate, nicnum, fname, lname, phone, from, to, seatnum, busnum, time);
+    const newBookingsDocRef = await bookingsModel.add(newBookings.toFirebaseData());
+    const newBookingsDoc = await newBookingsDocRef.get();
+
+    res.send({ status: 200, message: 'Bookings added successfully', BookingsDetails: newBookingsDoc.data() });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ status: 400, message: 'Unable to Add Bookings' });
+  }
+});
 
 
 module.exports = router;
