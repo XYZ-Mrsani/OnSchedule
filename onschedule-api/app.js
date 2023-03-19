de675748e48a3cdbc76ef7da72703b0b4d2585c0
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-19 09:38:19 +0530

    create bus route

--- onschedule-api/app.js
+++ onschedule-api/app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 
 var bookingsRouter = require('./routes/bookings');
 var adminRouter = require('./routes/admin');
+var busRouter = require('./routes/bus');
 var app = express();
 app.use(cors())
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/', indexRouter);
 app.use('/admin', adminRouter);
 app.use('/bookings', bookingsRouter);
+app.use('/bus', busRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
