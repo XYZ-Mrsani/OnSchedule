4a453aff013fad177df0f9d1da03f139443c7735
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-22 11:44:10 +0530

    add edit bookings route

--- onschedule-ui/onschedule-ui/src/app/app-routing.module.ts
+++ onschedule-ui/onschedule-ui/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { LogoutComponent } from './authentication/logout/logout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditBookingsComponent } from './dashboard/edit-bookings/edit-bookings.component';
 
 const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent},
-  {path:'',component:LoginComponent},
-  {path:'login',component:LoginComponent},
-  {path:'logout',component:LogoutComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'dashboard/edit/:id', component: EditBookingsComponent },
   
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'authentication', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
