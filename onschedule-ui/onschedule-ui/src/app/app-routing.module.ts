f0b0f6e5cb2675e8490752a667992e1c5d25e14b
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-25 20:21:37 +0530

    add deletebus route

--- onschedule-ui/onschedule-ui/src/app/app-routing.module.ts
+++ onschedule-ui/onschedule-ui/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login/login.component';
 import { LogoutComponent } from './authentication/logout/logout.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DeleteBusComponent } from './dashboard/delete-bus/delete-bus.component';
 import { EditBookingsComponent } from './dashboard/edit-bookings/edit-bookings.component';
 import { EditBusesComponent } from './dashboard/edit-buses/edit-buses.component';
 import { EditPassengerComponent } from './dashboard/edit-passenger/edit-passenger.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'dashboard/edit/:id', component: EditBookingsComponent },
   { path: 'dashboard/editbus/:id', component: EditBusesComponent },
   { path: 'dashboard/editfeedback/:id', component: EditPassengerComponent },
+  { path: 'dashboard/deletebus/:id', component: DeleteBusComponent },
 
   
   { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
