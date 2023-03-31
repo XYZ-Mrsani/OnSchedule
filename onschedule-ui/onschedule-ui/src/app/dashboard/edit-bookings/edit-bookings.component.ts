900929a2b4b46b6f14a85a8ed454edc7587cd5e6
Sandeepa <sandeepaathauda2002@gmail.com>
2023-03-31 18:04:02 +0530

    add js for seat selection

--- onschedule-ui/onschedule-ui/src/app/dashboard/edit-bookings/edit-bookings.component.ts
+++ onschedule-ui/onschedule-ui/src/app/dashboard/edit-bookings/edit-bookings.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { BookingsService } from '../../services/bookings.service';
 
 @Component({
@@ -6,21 +6,74 @@ import { BookingsService } from '../../services/bookings.service';
   templateUrl: './edit-bookings.component.html',
   styleUrls: ['./edit-bookings.component.css']
 })
-export class EditBookingsComponent implements OnInit {
+export class EditBookingsComponent implements OnInit, AfterViewInit {
 
-  vBookingsDetails:any;
-  vBookingsList:any;
+  vBookingsDetails: any;
+  vBookingsList: any;
 
-  constructor(private bookingService:BookingsService){}
+  constructor(private bookingService: BookingsService) { }
 
   ngOnInit(): void {
     this.viewBookings();
+
+    /**JS */
+
+
+    /** */
   }
 
-  viewBookings(){
+  ngAfterViewInit(): void {
+    const container = document.querySelector('.container-m');
+    const seats = Array.from(document.querySelectorAll('.row .seat:not(.occupied'));
+
+    populateUI();
+
+
+    // update total and count
+    function updateSelectedCount() {
+      const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+      const seatsIndex = Array.from(selectedSeats).map((seat) => seats.indexOf(seat));
+
+      localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    }
+
+    // get data from localstorage and populate ui
+    function populateUI() {
+      const selectedSeats = localStorage.getItem('selectedSeats');
+      if (selectedSeats !== null) {
+        const prasedSeats = JSON.parse(selectedSeats);
+        if (prasedSeats.length > 0) {
+          seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index.toString()) > -1) {
+              seat.classList.add('selected');
+            }
+          });
+        }
+      }
+    }
+
+
+    // Seat click event
+    if (container) {
+      container.addEventListener('click', (e) => {
+        const target = e.target as HTMLElement;
+        if (target && target.classList.contains('seat') && !target.classList.contains('occupied')) {
+          target.classList.toggle('selected');
+
+          updateSelectedCount();
+        }
+      });
+    }
+
+    // intial count and total
+    updateSelectedCount();
+  }
+
+  viewBookings() {
     let id = window.location.pathname.split("/").pop();
 
-    this.bookingService.viewBookings(id).subscribe(data=>{
+    this.bookingService.viewBookings(id).subscribe(data => {
       this.vBookingsDetails = data;
       this.vBookingsList = this.vBookingsDetails.results;
       console.log(this.vBookingsList);
