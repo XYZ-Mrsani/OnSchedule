7032e92bf0acaed6525874dee6feb8c76dd31e60
Sandeepa <sandeepaathauda2002@gmail.com>
2023-05-19 15:41:56 +0530

    add booking service and env

--- onschedule-ui/onschedule-ui/src/app/services/bookings.service.ts
+++ onschedule-ui/onschedule-ui/src/app/services/bookings.service.ts
@@ -7,34 +7,44 @@ import { environment } from 'src/environments/environment.dev';
 })
 export class BookingsService {
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getBookings(){
-    let url = environment.Bookings_BASE_URL+environment.BOOKINGS.GET_ALL_BOOKINGS;
+  getBookings() {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.GET_ALL_BOOKINGS;
     return this.httpClient.get(url);
   }
 
-  getCancelBookings(){
-    let url = environment.Bookings_BASE_URL+environment.BOOKINGS.GET_ALL_CANCEL_BOOKINGS;
+  getCancelBookings() {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.GET_ALL_CANCEL_BOOKINGS;
     return this.httpClient.get(url);
   }
 
-  viewBookings(id){
-    let url = environment.Bookings_BASE_URL+environment.BOOKINGS.VIEW_BOOKINGS+id;
+  viewBookings(id) {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.VIEW_BOOKINGS + id;
     return this.httpClient.get(url);
   }
-  dBookings(id){
+  dBookings(id) {
     let url = environment.Bookings_BASE_URL + environment.BOOKINGS.DELETE + id;
     return this.httpClient.delete(url);
   }
 
-  deleteBookings(){
-    let url = environment.Bookings_BASE_URL+environment.BOOKINGS.DELETE_BOOKINGS;
+  deleteBookings() {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.DELETE_BOOKINGS;
     return this.httpClient.delete(url);
   }
 
-  editBookings(id, datetime, nicnum, fname, lname, phone, from, to, seatnum, busnum, amount, time){
-    let url = environment.Bookings_BASE_URL+environment.BOOKINGS.UPDATE+id;
-    return this.httpClient.put(url, {datetime, nicnum, fname, lname, phone, from, to, seatnum, busnum, amount, time})
+  editBookings(id, datetime, nicnum, fname, lname, phone, from, to, seatnum, busnum, amount, time) {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.UPDATE + id;
+    return this.httpClient.put(url, { datetime, nicnum, fname, lname, phone, from, to, seatnum, busnum, amount, time })
+  }
+
+  vBooking(nicnum) {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.V_BOOKING + nicnum;
+    return this.httpClient.get(url);
+  }
+
+  vCancel(nicnum) {
+    let url = environment.Bookings_BASE_URL + environment.BOOKINGS.V_C_BOOKING + nicnum;
+    return this.httpClient.get(url);
   }
 }
--- onschedule-ui/onschedule-ui/src/environments/environment.dev.ts
+++ onschedule-ui/onschedule-ui/src/environments/environment.dev.ts
@@ -8,7 +8,9 @@ export const environment = {
         VIEW_BOOKINGS: 'viewbookings?id=',
         DELETE: 'delete?id=',
         DELETE_BOOKINGS: 'deletebookings',
-        UPDATE: 'update?id='
+        UPDATE: 'update?id=',
+        V_BOOKING: 'vbooking?nicnum=',
+        V_C_BOOKING: 'vcancel?nicnum='
 
     },
     BUSES_BASE_URL: 'http://localhost:3000/bus/',
@@ -18,12 +20,14 @@ export const environment = {
         VIEW_BUS: 'viewbus?id=',
         UPDATE: 'update?id=',
         DELETE: 'delete?id=',
-        VBUS: 'vbus?busnum='
+        VBUS: 'vbus?busnum=',
+        UPDATE_SEAT: 'updateseat'
     },
 
     TRANSACTION_BASE_URL: 'http://localhost:3000/transaction/',
     TRANSACTION: {
-        GET_ALL_TRANSACTION: 'list'
+        GET_ALL_TRANSACTION: 'list',
+        VIEW_TRANSACTION: 'viewt?nicnum='
     },
     ADMIN_BASE_URL: 'http://localhost:3000/admin/',
     ADMIN: {
@@ -34,6 +38,7 @@ export const environment = {
         GET_ALL_FEEDBACK: 'list',
         VIEW_FEEDBACK: 'viewfeedback?id=',
         UPDATE: 'update?id=',
-        DELETE: 'delete?id='
+        DELETE: 'delete?id=',
+        V_FEEDBACK: 'vfeedback?busnum='
     }
 };
